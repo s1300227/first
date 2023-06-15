800aefeda3dc5ced333a5df2cf0b70bc053df091
s1300227 <136040749+s1300227@users.noreply.github.com>
2023-06-15 13:19:35 +0900

    Greetings added

--- pr2.c
+++ pr2.c
@@ -9,13 +9,6 @@ int main(){
     scanf("%s",name);
     printf("Hello, %s!\n",name);
   
-    srand((unsigned int)time(NULL));
-    printf("Rolling dice. . .\n");
-    for(int i=0;i<2;i++){
-      num = rand()%6+1;
-      printf("Die %d: %d\n",i+1,num);
-      sum+=num;
-    }
-    printf("Total value: %d\n",sum);
+   
 return 0;  
 }
