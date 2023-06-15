65e2718632f58536360fe931b72c8e01178c4cca
s1300227 <136040749+s1300227@users.noreply.github.com>
2023-06-15 15:20:40 +0900

    Greetings added

--- pr2.c
+++ pr2.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+
 int main(){
-   int num;
-    int sum =0;
+   
     char name[10];
     printf("What is your name?\n");
     scanf("%s",name);
