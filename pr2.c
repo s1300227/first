5e2f4d8563f97276e615cfe06a3de6e8786e3318
s1300227 <136040749+s1300227@users.noreply.github.com>
2023-06-15 11:35:21 +0900

    Greetings added

--- pr2.c
+++ pr2.c
@@ -2,14 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 int main(){
-  int num;
-  int sum =0;
-  srand((unsigned int)time(NULL));
-  printf("Rolling dice. . .\n");
-  for(int i=0;i<2;i++){
-    num = rand()%6+1;
-    printf("Die %d: %d\n",i+1,num);
-    sum+=num;
-  }
-  printf("Total value: %d\n",sum);
+   int num;
+    int sum =0;
+    char name[10];
+    printf("What is your name?\n");
+    scanf("%s",name);
+    printf("Hello, %s!\n",name);
+  
+    srand((unsigned int)time(NULL));
+    printf("Rolling dice. . .\n");
+    for(int i=0;i<2;i++){
+      num = rand()%6+1;
+      printf("Die %d: %d\n",i+1,num);
+      sum+=num;
+    }
+    printf("Total value: %d\n",sum);
+return 0;  
 }
